2d20c929d5663678e3f8c2cfe3789fd221905303
Anton <antonprok86@gmail.com>
2021-12-12 15:45:47 +0300

    fix gitignore

--- lib/resources/superheroes_images.dart
+++ lib/resources/superheroes_images.dart
@@ -2,7 +2,7 @@ class SuperheroesImages {
   SuperheroesImages._();
 
   static const ironman = "assets/images/ironman.png";
-  static const halk = "assets/images/halk.png";
+  static const hulk = "assets/images/hulk.png";
   static const superman = "assets/images/superman.png";
   static const batmanUrl =
       "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg";
